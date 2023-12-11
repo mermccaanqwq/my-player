cd1b2b55d02c3bd57503ca27bd1146df189ce4d1
Mitrofanov Kirill <119688216+mermccaanqwq@users.noreply.github.com>
2023-12-11 12:30:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const audioPlayer = document.getElementById('audioPlayer');
+
+    audioPlayer.addEventListener('play', function () {
+        console.log('Playing...');
+    });
+
+    audioPlayer.addEventListener('pause', function () {
+        console.log('Paused...');
+    });
+
+    audioPlayer.addEventListener('ended', function () {
+        console.log('Track finished...');
+    });
+});
 
